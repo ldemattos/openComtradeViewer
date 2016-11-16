14717afbe4edf760bddd4c0950cacd284e18a07b
ldemattos <l@mattos.eng.br>
2016-11-16 09:27:47 -0200

    Giving the deserved acknowledgment to Moreto's pyComtrade work

--- src/GUI_About.py
+++ src/GUI_About.py
@@ -34,7 +34,7 @@ def aboutWin():
 	
 	# set about window dimensions and position
 	w = 400
-	h = 120
+	h = 190
 	about.resizable(width='false', height='false')
 	width = about.winfo_screenwidth()
 	height = about.winfo_screenheight()
@@ -46,6 +46,10 @@ def aboutWin():
 	tkinter.Label(about,text="openComtradeViewer", font=12).grid(row=0,column=0)
 	tkinter.Label(about,text="Copyright 2016").grid(row=1,column=0)
 	tkinter.Label(about,text="Leonardo M. N. de Mattos <l@mattos.eng.br>").grid(row=2,column=0)
-	tkinter.Label(about,text="Aniela M. P. Mendes <anielampm@protonmail.ch>").grid(row=3,column=0)
-	tkinter.Label(about,text="Project page: http://github.com/ldemattos/openComtradeViewer").grid(row=4,column=0)
-	tkinter.Label(about,text="License: GNU GPL v3").grid(row=5,column=0)	
+	tkinter.Label(about,text="Project page: http://github.com/ldemattos/openComtradeViewer").grid(row=3,column=0)
+	tkinter.Label(about,text="License: GNU GPL v3").grid(row=4,column=0)
+	tkinter.Label(about,text="pyComtrade", font=12).grid(row=5,column=0)
+	tkinter.Label(about,text="Copyright 2013").grid(row=6,column=0)
+	tkinter.Label(about,text="Miguel Moreto <miguel.moreto@ufsc.br>").grid(row=7,column=0)
+	tkinter.Label(about,text="Project page: http://github.com/miguelmoreto/pycomtrade").grid(row=8,column=0)
+	tkinter.Label(about,text="License: GNU GPL v3").grid(row=9,column=0)
